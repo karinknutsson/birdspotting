98f8cd77be247e3b3dc09ce42a0d4903e08dde09
karin knutsson <karinmiriam@gmail.com>
2021-02-23 16:40:23 +0100

    added username and bio to users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'about', to: 'pages#about'
+
   resources :birds do
     resources :spots
   end
