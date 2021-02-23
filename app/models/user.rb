a5d88859117391eb3be13d16a65fb0916957ac98
karin knutsson <karinmiriam@gmail.com>
2021-02-23 16:45:37 +0100

    added permitted parameters for username, bio and set validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :spots
+
+  validates :username, uniqueness: true
+  validates :username, length: { in: 2..15 }
 end
