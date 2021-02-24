4cec6a627afb9bc2a61a6047daa7ad21ab8ecdc8
karin knutsson <karinmiriam@gmail.com>
2021-02-24 11:13:28 +0100

    added actions in birds and spots controllers

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,44 @@
 class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+  end
+
+  def show
+    @bird = Bird.find(params[:id])
+  end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+    if @bird.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bird = Bird.find(params[:id])
+  end
+
+  def update
+    @bird = Bird.find(params[:id])
+    @bird.update(bird_params)
+    redirect_to bird_path(@bird)
+  end
+
+  def destroy
+    @bird = Bird.find(params[:id])
+    @bird.destroy
+    redirect_to birds_path
+  end
+
+  private
+
+  def bird_params
+    params.require(:birds).permit(:name, :latin_name, :image, :description)
+  end
 end
--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,51 @@
 class SpotsController < ApplicationController
+  def index
+    @spots = Spot.all
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  def new
+    set_bird
+    @spot = Spot.new
+  end
+
+  def create
+    set_bird
+    @spot = Spot.new(spot_params)
+    @spot.bird = @bird
+    if @spot.save
+      redirect_to spots_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @spot = Spot.find(params[:id])
+  end
+
+  def update
+    @spot = Spot.find(params[:id])
+    @spot.update(spot_params)
+    redirect_to spot_path(@spot)
+  end
+
+  def destroy
+    @spot = Spot.find(params[:id])
+    @spot.destroy
+    redirect_to spots_path
+  end
+
+  private
+
+  def set_bird
+    @bird = Bird.find(params[:bird_id])
+  end
+
+  def spot_params
+    params.require(:spots).permit(:date, :time, :location, :number, :bird_id, :user_id)
+  end
 end
