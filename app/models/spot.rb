1af9d0ed93039bb45d149ab1513bd4a1a11a4c69
karin knutsson <karinmiriam@gmail.com>
2021-04-30 10:14:08 +0200

    created form for new spot with location translating to GPS coordinates

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,6 @@ class Spot < ApplicationRecord
   belongs_to :bird
   belongs_to :user
 
-  # geocoded_by :location
-  # after_validation :geocode, if: :will_save_change_to_location?
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
