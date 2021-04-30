7d5798ea9e9c0c5fe8232eacf64003b862294306
karin knutsson <karinmiriam@gmail.com>
2021-04-30 14:34:10 +0200

    note table added to spots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_165432) do
+ActiveRecord::Schema.define(version: 2021_04_30_123324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_165432) do
     t.date "spot_date"
     t.float "latitude"
     t.float "longitude"
+    t.text "note"
     t.index ["bird_id"], name: "index_spots_on_bird_id"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
