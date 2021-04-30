f6e80842a4838388ce4dc97645d89e8f81dfd017
karin knutsson <karinmiriam@gmail.com>
2021-04-30 10:48:21 +0200

    added locations to seeds

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,9 @@ class Spot < ApplicationRecord
   belongs_to :bird
   belongs_to :user
 
+  validates :bird_id, presence: true
+  validates :number, presence: true
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
