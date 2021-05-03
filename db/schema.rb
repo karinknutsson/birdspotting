ed6232b8e2f921d159f63fbfbe9d34261352694c
karin knutsson <karinmiriam@gmail.com>
2021-05-03 18:54:30 +0200

    created wikipedia scraper for bird species

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_123324) do
+ActiveRecord::Schema.define(version: 2021_05_03_140822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_04_30_123324) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "credit"
+    t.string "wiki_name"
   end
 
   create_table "spots", force: :cascade do |t|
---
+++ scraper.rb
@@ -0,0 +1,66 @@
+require 'open-uri'
+require 'nokogiri'
+
+require 'pry-byebug'
+
+def make_name_wiki(bird_name)
+  parts = bird_name.split(' ')
+  parts_down = parts.map &:downcase
+  parts_down[0].capitalize!
+  parts_down.join('_').gsub(/'/, '%27')
+end
+
+def scrape_wiki(wiki_name)
+  html_content = open("https://en.wikipedia.org/wiki/#{wiki_name}").read
+  doc = Nokogiri::HTML(html_content)
+  latin_name = doc.search('.infobox').at_css('span.binomial').text.strip
+
+  wiki_content = doc.search('.mw-parser-output')
+  wiki_children = wiki_content.children
+  first_n = []
+
+  i = 3
+  while i < 20 do
+    if wiki_children[i].attributes["id"] && wiki_children[i].attributes["id"].content == "toc"
+      break
+    end
+    first_n << wiki_children[i]
+    i += 1
+  end
+
+  first_n.reverse!
+  paragraphs = []
+
+  j = 0
+  while j < first_n.length do
+    if first_n[j].attributes["class"] && first_n[j].attributes["class"].content == "infobox biota"
+      break
+    end
+    paragraphs << first_n[j]
+    j += 1
+  end
+
+  paragraphs.reverse!
+
+  description = ""
+  paragraphs.each do |p|
+    description += p.text.strip
+  end
+  description.gsub!(/\[\d\]/, '')
+
+  return [latin_name, description]
+end
+
+def scrape_and_print(bird)
+  wiki_name = make_name_wiki(bird)
+  p wiki_name + ":"
+  p scrape_wiki(wiki_name)
+end
+
+bird = 'euraSian MAGPIE'
+scrape_and_print(bird)
+
+bird = "WILSON'S STORM PETREL"
+scrape_and_print(bird)
+
+# name = element.search('.brand-bg').search('h1').text.strip
