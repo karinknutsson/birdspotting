9e635f9e172ea96b3fa9da7061296152bd21f95a
karin knutsson <karinmiriam@gmail.com>
2021-05-03 09:08:39 +0200

    map controls added, map slightly zoomed out on spot page

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -8,7 +8,6 @@ if (mapElement) {
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/karinmiriam/cko88xcgl4vju18pgby8zpan6'
-    // style: 'mapbox://styles/mapbox/streets-v10'
   });
 
   const markers = JSON.parse(mapElement.dataset.markers);
@@ -17,4 +16,21 @@ if (mapElement) {
     .setLngLat([ marker.lng, marker.lat ])
     .addTo(map);
   });
+
+  if (markers.length === 0) {
+    map.setZoom(0);
+  } else if (markers.length === 1) {
+    map.setZoom(12);
+    map.setCenter([markers[0].lng, markers[0].lat]);
+  } else {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    map.fitBounds(bounds, { duration: 0, padding: 75})
+  }
+
+  map.addControl(new mapboxgl.NavigationControl());
 }
+
+
