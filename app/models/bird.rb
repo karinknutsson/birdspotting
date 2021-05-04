25ed200534b46c7a3e891ffc1909fc90e0d10542
karin knutsson <karinmiriam@gmail.com>
2021-05-04 13:38:42 +0200

    added validations to bird model and set default photo for daily  bird when image is not attached

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -3,6 +3,6 @@ class Bird < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  # validates :latin_name, presence: true, uniqueness: true
-  # validates :wiki_name, presence: true, uniqueness: true
+  validates :latin_name, presence: true, uniqueness: true
+  validates :wiki_name, presence: true, uniqueness: true
 end
