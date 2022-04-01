7006fa3aaeb24b3b1005b5e9d0b9f61319052370
karin knutsson <karinmiriam@gmail.com>
2022-04-01 11:06:00 +0200

    created file and set up route for life list

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
 
   get '/profile/:id', to: 'pages#profile', as: 'profile'
   get 'settings', to: 'pages#settings', as: 'settings'
+  get '/life-list/:id', to: 'pages#life-list', as: 'life_list'
 end
