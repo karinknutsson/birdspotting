7029500ad54158800b8402e495e7886685ed3bda
karin knutsson <karinmiriam@gmail.com>
2022-04-06 17:32:20 +0200

    finished test for bird list

--- test/system/birds_test.rb
+++ test/system/birds_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class BirdsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit birds_url
-    assert_selector("div.white-auto-container")
+    login_as users(:marge)
+    visit "/birds"
+    assert_selector("li.bird", count: Bird.count)
   end
 end
