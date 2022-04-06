a254bca8e54b37bce6168b6bdc4171fd61777dfc
karin knutsson <karinmiriam@gmail.com>
2022-04-06 18:13:04 +0200

    system test for spots

--- test/system/spots_test.rb
+++ test/system/spots_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class SpotsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit spots_url
-  #
-  #   assert_selector "h1", text: "Spot"
-  # end
+  test "visiting the index" do
+    login_as users(:marge)
+    visit "/users/1/spots"
+    assert_selector("li.spot", count: Spot.count)
+  end
 end
